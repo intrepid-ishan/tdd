faa1643c6da6667450861479e4178d2fad2a1d9f
Ishan Makadia <i5han3.dev@gmail.com>
2023-02-23 00:50:46 -0400

    feat (test-4): create stack and use in top and push method

--- src/main/java/MinMonotonicStack.java
+++ src/main/java/MinMonotonicStack.java
@@ -1,12 +1,26 @@
+import java.util.ArrayList;
 import java.util.EmptyStackException;
+import java.util.List;
 
 public class MinMonotonicStack {
+    private List<Integer> stack;
+
+    public MinMonotonicStack() {
+        stack = new ArrayList<>();
+    }
+
     public int push(int element) {
+        stack.add(element);
+
         return element;
     }
 
     public int top() {
-        throw new EmptyStackException();
+        if(stack.isEmpty()){
+            throw new EmptyStackException();
+        }
+
+        return stack.get(stack.size() - 1);
     }
 
     public int pop() {
