fcf83a2d61a5a1d00f215d12c57f0587af129d10
Meet Sinojia <meetsinojia7@gmail.com>
2023-02-23 00:02:41 -0400

    Test: Empty top

--- src/test/java/MinMonotonicStackTest.java
+++ src/test/java/MinMonotonicStackTest.java
@@ -1,3 +1,19 @@
-public class MinMonotonicStackTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import java.util.EmptyStackException;
+
+public class MinMonotonicStackTest {
+    private MinMonotonicStack stack;
+
+    @BeforeEach
+    void setupMinMonotonicStack() {
+        stack = new MinMonotonicStack();
+    }
+
+    @Test
+    void emptyTop(){
+        Assertions.assertThrows(EmptyStackException.class, stack::top);
+    }
 }
