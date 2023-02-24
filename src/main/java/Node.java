19952623f11e33fcce45322ddfa6c785bad50661
Meet Sinojia <meetsinojia7@gmail.com>
2023-02-24 01:47:46 -0400

    feat (test-2): implement Node class and add node to result while traversing

--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -1,2 +1,9 @@
 public class Node {
+    int val;
+    Node left;
+    Node right;
+
+    Node(int val) {
+        this.val = val;
+    }
 }
--- src/main/java/PostOrderTraversal.java
+++ src/main/java/PostOrderTraversal.java
@@ -4,6 +4,14 @@ import java.util.List;
 public class PostOrderTraversal {
     public static List<Integer> traverse(Node root, boolean recursive) {
         List<Integer> result = new ArrayList<>();
+        recursiveTraversal(root, result);
         return result;
+    }
+
+    private static void recursiveTraversal(Node node, List<Integer> result) {
+        if(node == null) {
+            return;
+        }
+        result.add(node.val);
     }
 }
