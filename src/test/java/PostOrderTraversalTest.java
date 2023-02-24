12066d2ddcfb8e3d971ce133dccb9e58eb935327
Ishan Makadia <i5han3.dev@gmail.com>
2023-02-24 01:32:08 -0400

    test (1): add empty tree recursive test case

--- src/test/java/PostOrderTraversalTest.java
+++ src/test/java/PostOrderTraversalTest.java
@@ -1,3 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PostOrderTraversalTest {
-    
+    @Test
+    public void testRecursiveEmptyTree() {
+        Node root = null;
+
+        List<Integer> result = PostOrderTraversal.traverse(root, true);
+
+        List<Integer> expected = new ArrayList<>();
+        assertEquals(expected, result);
+    }
 }
