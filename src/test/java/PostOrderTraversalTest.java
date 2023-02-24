a98a583d2bc723c443a5bce1f0ad5e0171ad4e7c
Ishan Makadia <i5han3.dev@gmail.com>
2023-02-24 02:00:17 -0400

    test (4): add complete tree case

--- src/test/java/PostOrderTraversalTest.java
+++ src/test/java/PostOrderTraversalTest.java
@@ -38,4 +38,20 @@ public class PostOrderTraversalTest {
         List<Integer> expected = Arrays.asList(7, 6, 5);
         assertEquals(expected, result);
     }
+
+    @Test
+    public void testRecursiveCompleteTree() {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        List<Integer> result = PostOrderTraversal.traverse(root, true);
+
+        List<Integer> expected = Arrays.asList(4, 5, 2, 6, 7, 3, 1);
+        assertEquals(expected, result);
+    }
 }
