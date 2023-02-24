011c762065b21eed27d308c2231c5c8319d7d7e3
Meet Sinojia <mt845464@dal.ca>
2023-02-24 01:40:24 -0400

    feat (test-1): return empty list

--- src/main/java/PostOrderTraversal.java
+++ src/main/java/PostOrderTraversal.java
@@ -1,3 +1,9 @@
-public class PostOrderTraversal {
+import java.util.ArrayList;
+import java.util.List;
 
+public class PostOrderTraversal {
+    public static List<Integer> traverse(Node root, boolean recursive) {
+        List<Integer> result = new ArrayList<>();
+        return result;
+    }
 }
