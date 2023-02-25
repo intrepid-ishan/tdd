056355fcf915817dbdc5b0e28befe7c12aca174d
Ishan Makadia <i5han3.dev@gmail.com>
2023-02-24 23:42:58 -0400

    test (8): add iterative complete tree

--- src/test/java/PostOrderTraversalTest.java
+++ src/test/java/PostOrderTraversalTest.java
@@ -86,4 +86,20 @@ public class PostOrderTraversalTest {
         List<Integer> expected = Arrays.asList(7, 6, 5);
         assertEquals(expected, result);
     }
+
+    @Test
+    public void testIterativeCompleteTree() {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        List<Integer> result = PostOrderTraversal.traverseIteratively(root);
+
+        List<Integer> expected = Arrays.asList(4, 5, 2, 6, 7, 3, 1);
+        assertEquals(expected, result);
+    }
 }
